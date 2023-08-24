bfe271d75db144e17bbef6c61efb80be5207c49a
Bifabig <biftubig@gmail.com>
2023-08-24 11:49:46 +0300

    add new posts form

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show]
   end
 
+  get 'users/:user_id/posts/:post_id/comments/new', to: 'comments#new'
+  post 'users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'comments'
   
 end
